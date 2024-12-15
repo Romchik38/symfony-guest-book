d3549dc602a94bea169678d45cb65c1dbdfa45c1
Romchik38 <pomahehko.c@gmail.com>
2024-12-15 12:07:49 +0200

    added method

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
+    #[Route('/', name: 'homepage', methods: ['GET'])]
     public function index(): Response
     {
         $html = 
