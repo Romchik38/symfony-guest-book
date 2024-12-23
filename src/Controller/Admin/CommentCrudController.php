87ecfde2167f9e4d5a23e285eab1e4bec5cbba24
Romchik38 <pomahehko.c@gmail.com>
2024-12-23 13:12:10 +0200

    added a few comments and conference

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -4,9 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Comment;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 
 class CommentCrudController extends AbstractCrudController
 {
@@ -15,14 +13,13 @@ class CommentCrudController extends AbstractCrudController
         return Comment::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+
+        $fields = parent::configureFields($pageName);
+        $fields[] = AssociationField::new('conference');
+        return $fields;
     }
-    */
+
 }
