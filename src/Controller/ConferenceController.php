a705f7296ce90373ea506137de9550ee5519221e
Romchik38 <pomahehko.c@gmail.com>
2024-12-24 11:54:18 +0200

    added twig/intl-extra

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,7 +12,8 @@ use Symfony\Component\Routing\Attribute\Route;
 class ConferenceController extends AbstractController
 {
     public function __construct(
-        protected readonly ConferenceRepository $conferenceRepository
+        protected readonly ConferenceRepository $conferenceRepository,
+        protected readonly CommentRepository $commentRepository
     ) {}
 
     #[Route('/', name: 'homepage', methods: ['GET'])]
@@ -23,5 +26,17 @@ class ConferenceController extends AbstractController
                 'conferences' => $this->conferenceRepository->findAll(),
             ]
         );
+    }
+
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(Conference $conference): Response
+    {
+        return $this->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $this->commentRepository->findBy(
+                ['conference' => $conference],
+                ['createdAt' => 'DESC']
+            ),
+        ]);
     }
 }
