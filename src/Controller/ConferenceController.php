e0c2530b6bd4249d642c665e364248aaab01e79a
Romchik38 <pomahehko.c@gmail.com>
2024-12-24 11:40:58 +0200

    conference view in progress

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,25 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ConferenceController extends AbstractController
 {
+    public function __construct(
+        protected readonly ConferenceRepository $conferenceRepository
+    ) {}
+
     #[Route('/', name: 'homepage', methods: ['GET'])]
     public function index(): Response
     {
-        $html = 
-        <<<EOF
-            <html>
-                <body>
-                    <h1>Home page</h1>
-                    <img src="/img/young-woman-say-hello-isolated-white-wall.jpg" />
-                </body>
-            </html>
-        EOF;
 
-        return new Response($html);
+        return $this->render(
+            'conference/index.html.twig',
+            [
+                'conferences' => $this->conferenceRepository->findAll(),
+            ]
+        );
     }
 }
