8d40a15660eabfa35c2816f265fdc967b61e6c8a
Romchik38 <pomahehko.c@gmail.com>
2024-12-24 12:27:40 +0200

    added pagination

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -6,6 +6,7 @@ use App\Entity\Conference;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -29,14 +30,19 @@ class ConferenceController extends AbstractController
     }
 
     #[Route('/conference/{id}', name: 'conference')]
-    public function show(Conference $conference): Response
+    public function show(Request $request, Conference $conference): Response
     {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $this->commentRepository->getCommentPaginator($conference, $offset);
+
+        $previous = $offset - CommentRepository::COMMENTS_PER_PAGE;
+        $next = min(count($paginator), $offset + CommentRepository::COMMENTS_PER_PAGE);
+
         return $this->render('conference/show.html.twig', [
             'conference' => $conference,
-            'comments' => $this->commentRepository->findBy(
-                ['conference' => $conference],
-                ['createdAt' => 'DESC']
-            ),
+            'comments' => $paginator,
+            'previous' => $previous,
+            'next' => $next,
         ]);
     }
 }
